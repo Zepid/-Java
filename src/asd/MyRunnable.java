ccec6d19f325fcab17c25a93dcb968376fce7692
Zepid <1120658265@qq.com>
2019-11-03 18:55:31 +0800

    commit

---
+++ src/asd/MyRunnable.java
@@ -0,0 +1,15 @@
+package asd;
+
+public class MyRunnable implements Runnable { 
+	public void run() { 
+		for (int i = 0; i < 50; i++) {
+			System.out.println("new");
+		}
+	} 
+	public static void main(String[] args) { 
+		new Thread(new MyRunnable()).start(); 
+		for (int i = 0; i < 100; i++) {
+			System.out.println("main");
+		}
+	}
+}
